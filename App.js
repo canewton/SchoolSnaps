189ba627438bded570c9621c2d52643480c0adb8
1canewton <1canewton@seattleschools.org>
2021-07-14 11:34:08 -0700

    Add Basic Navigation

--- App.js
+++ App.js
@@ -1,13 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./src/screens/HomeScreen"
+import CalendarScreen from "./src/screens/CalendarScreen"
+import LibraryScreen from "./src/screens/LibraryScreen"
+
+const HomeStack = createStackNavigator();
+const CalendarStack = createStackNavigator();
+const LibraryStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+//edit the default theme to customize the background color and header color
+const Theme = {
+  dark: false,
+  colors: {
+    //make the back button white
+    primary: "white",
+    //make the background color an off-white
+    background: "rgb(242, 242, 242)",
+    //make the header color white
+    card: "#ffffff",
+    //make the header text black
+    //text: "black",
+  },
+};
+
+const HomeStackScreen = () =>{
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+      />
+    </HomeStack.Navigator>
+  )
+}
+
+const CalendarStackScreen = () =>{
+  return (
+    <CalendarStack.Navigator>
+      <CalendarStack.Screen
+        name="Calendar"
+        component={CalendarScreen}
+      />
+    </CalendarStack.Navigator>
+  )
+}
+
+const LibraryStackScreen = () =>{
+  return (
+    <LibraryStack.Navigator>
+      <LibraryStack.Screen
+        name="Library"
+        component={LibraryScreen}
+      />
+    </LibraryStack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={Theme}>
+      <Tab.Navigator>
+        <Tab.Screen
+          name = "Home"
+          component = {HomeStackScreen}
+        />
+        <Tab.Screen
+          name = "Calendar"
+          component = {CalendarStackScreen}
+        />
+        <Tab.Screen
+          name = "Library"
+          component = {LibraryStackScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/LibraryScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const LibraryScreen = () => {
+  return (
+    <View>
+      <Text>Library Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default LibraryScreen;
