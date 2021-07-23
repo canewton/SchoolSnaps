79c6eb765aaf91fca09ab88a8e447e06aed41dfd
1canewton <1canewton@seattleschools.org>
2021-07-22 22:24:30 -0700

    Bottom Sheet Working

--- src/screens/NotesScreen.js
+++ src/screens/NotesScreen.js
@@ -1,24 +1,27 @@
 import { useNavigation } from "@react-navigation/core";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import BackButton from "../components/BackButton";
 import FloatingActionButton from "../components/FloatingActionButton";
 import { GeneralIcons } from "../icons/GeneralIcons";
 import NotesList from "../components/NotesList";
 import { Context as NotesContext } from "../context/NotesContext";
-import Animated from "react-native-reanimated";
-import BottomSheet from "reanimated-bottom-sheet";
+import RBSheet from "react-native-raw-bottom-sheet";
+
+const bottomSheetHeight = 230;
 
 const NotesScreen = ({ route }) => {
   const navigation = useNavigation();
   const notes = useContext(NotesContext);
-  const sheetRef = React.useRef(null);
+  const sheetRef = React.useRef();
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => <BackButton color={route.params.primaryColor} />,
       headerRight: () => (
         <TouchableOpacity
-          onPress={() => navigation.navigate("Edit Class", route.params)}
+          onPress={() => {
+            sheetRef.current.open();
+          }}
           style={{ paddingRight: 15 }}
         >
           {GeneralIcons.findIcon("Edit", 24, route.params.primaryColor)}
@@ -31,6 +34,44 @@ const NotesScreen = ({ route }) => {
     <View style={{ flex: 1 }}>
       <NotesList notesFilteredByDate={notes.state} />
       <FloatingActionButton schoolClass={route.params} navigation={navigation} />
+      <RBSheet ref={sheetRef} closeOnDragDown={false} height={290} openDuration={250}>
+        <BottomSheetContent
+          navigation={navigation}
+          schoolClass={route.params}
+          onPressButton={() => {
+            sheetRef.current.close();
+          }}
+        />
+      </RBSheet>
+    </View>
+  );
+};
+
+const BottomSheetContent = ({ navigation, schoolClass, onPressButton }) => {
+  return (
+    <View style={{ backgroundColor: "white" }}>
+      <TouchableOpacity>
+        <Text style={styles.bottomSheetText}>Select notes</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.bottomSheetText}>Filter notes</Text>
+      </TouchableOpacity>
+      <View style={styles.border} />
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Edit Note", schoolClass);
+          onPressButton();
+        }}
+        style={styles.button}
+      >
+        <Text style={styles.bottomSheetText}>Edit class</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.bottomSheetText}>Archive class</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.bottomSheetText}>Delete class</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -44,6 +85,18 @@ const IconNextToText = (title, iconName) => {
   </View>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  border: {
+    borderBottomWidth: 3.5,
+    borderBottomColor: "#dee2e6",
+  },
+  bottomSheetText: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#343a40",
+    margin: 15,
+    alignItems: "flex-start",
+  },
+});
 
 export default NotesScreen;
