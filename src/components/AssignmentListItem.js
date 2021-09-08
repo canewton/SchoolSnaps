f4abdc3b7a7ec0bcef4dfdff1ef8e5e68ebc893a
1canewton <1canewton@seattleschools.org>
2021-09-08 15:34:10 -0700

    new assignment list item design

--- src/components/AssignmentListItem.js
+++ src/components/AssignmentListItem.js
@@ -1,8 +1,13 @@
 import React, { useRef, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Animated } from "react-native";
-import { AssignmentTypeIcons } from "../icons/AssignmentTypeIcons";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { GeneralIcons } from "../icons/GeneralIcons";
-import { Colors } from "../classes/Colors";
 
 const AssignmentListItem = ({ item, onPressCheckmark }) => {
   const [completed, setCompleted] = useState(item.completed);
@@ -21,43 +26,6 @@ const AssignmentListItem = ({ item, onPressCheckmark }) => {
     <Animated.View
       style={{ flexDirection: "row", justifyContent: "flex-end", opacity: opacity }}
     >
-      <View style={{ width: "10%" }}>
-        <TouchableOpacity
-          onPress={() => {
-            item.toggleCompleted();
-            setCompleted(item.completed);
-            onPressCheckmark(Animated.timing(opacityAnimationRef, fadeAnimation));
-          }}
-        >
-          {!completed && (
-            <View
-              style={{
-                ...styles.checkmarkCircle,
-                borderColor: item.schoolClass.primaryColor,
-              }}
-            />
-          )}
-          {completed && (
-            <View
-              style={{
-                ...styles.checkmarkCircle,
-                borderColor: item.schoolClass.primaryColor,
-                backgroundColor: item.schoolClass.primaryColor,
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              {GeneralIcons.findIcon("Check", 14, "white")}
-            </View>
-          )}
-        </TouchableOpacity>
-        <View
-          style={{
-            ...styles.dashedLine,
-            borderColor: item.schoolClass.primaryColor,
-          }}
-        />
-      </View>
       <TouchableOpacity
         onPress={() =>
           navigation.navigate("Edit Assignment", {
@@ -71,14 +39,26 @@ const AssignmentListItem = ({ item, onPressCheckmark }) => {
           backgroundColor: item.schoolClass.primaryColor,
         }}
       >
-        <View
-          style={{
-            ...styles.classIconContainer,
-            backgroundColor: Colors.changeOpacity("#000000", 0.1),
+        <TouchableWithoutFeedback
+          onPress={() => {
+            item.toggleCompleted();
+            setCompleted(item.completed);
+            onPressCheckmark(Animated.timing(opacityAnimationRef, fadeAnimation));
           }}
         >
-          {AssignmentTypeIcons.findIcon(item.iconName, 30, "white")}
-        </View>
+          <View
+            style={{
+              ...styles.checkmarkContainer,
+              borderColor: item.schoolClass.primaryColor,
+            }}
+          >
+            {completed && (
+              <View>
+                {GeneralIcons.findIcon("Check", 16, item.schoolClass.primaryColor)}
+              </View>
+            )}
+          </View>
+        </TouchableWithoutFeedback>
         <Text style={styles.assignmentText}>
           {item.schoolClass.name + " " + item.iconName}
         </Text>
@@ -92,7 +72,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: "white",
     marginTop: 10,
-    marginHorizontal: 10,
+    marginHorizontal: 15,
     flexDirection: "row",
     alignItems: "center",
     flex: 1,
@@ -106,13 +86,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginLeft: 12,
   },
-  checkmarkCircle: {
+  checkmarkContainer: {
     height: 20,
     width: 20,
-    borderRadius: 10,
-    borderWidth: 1.5,
-    marginTop: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
     marginLeft: 15,
+    alignItems: "center",
+    justifyContent: "center",
   },
   dashedLine: {
     borderWidth: 1,
@@ -123,7 +104,13 @@ const styles = StyleSheet.create({
     height: 55,
     borderRadius: 1,
   },
-  assignmentText: { fontSize: 20, fontWeight: "600", marginLeft: 10, color: "white" },
+  assignmentText: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginLeft: 15,
+    color: "white",
+    marginVertical: 15,
+  },
 });
 
 export default AssignmentListItem;
